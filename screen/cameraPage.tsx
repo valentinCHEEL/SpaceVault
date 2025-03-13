ca7e787a7fa5f51d60c4dd01e3b71d38f759c7a8
Mizer3 <91741197+Mizer3@users.noreply.github.com>
2025-03-13 15:31:35 +0100

    camera ok pas de save des images pour le moment

--- screen/cameraPage.tsx
+++ screen/cameraPage.tsx
@@ -1,74 +1,175 @@
-import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
-import { useNavigation } from 'expo-router';
-import { useState } from 'react';
-import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-
-export default function App() {
-  const [facing, setFacing] = useState<CameraType>('back');
-  const [permission, requestPermission] = useCameraPermissions();
-  const navigation = useNavigation();
-
-  if (!permission) {
-    // Camera permissions are still loading.
-    return <View />;
-  }
-
-  if (!permission.granted) {
-    // Camera permissions are not granted yet.
+import {
+    CameraMode,
+    CameraType,
+    CameraView,
+    useCameraPermissions,
+  } from "expo-camera";
+  import { useNavigation } from '@react-navigation/native';
+  import { useRef, useState } from "react";
+  import { Button, Pressable, StyleSheet, Text, View } from "react-native";
+  import { Image } from "expo-image";
+  import { AntDesign } from "@expo/vector-icons";
+  import { Feather } from "@expo/vector-icons";
+  import { FontAwesome6 } from "@expo/vector-icons";
+  
+  export default function App() {
+    const [permission, requestPermission] = useCameraPermissions();
+    const ref = useRef<CameraView>(null);
+    const [uri, setUri] = useState<string | null>(null);
+    const [mode, setMode] = useState<CameraMode>("picture");
+    const [facing, setFacing] = useState<CameraType>("back");
+    const [recording, setRecording] = useState(false);
+    const navigation = useNavigation();
+  
+    if (!permission) {
+      return null;
+    }
+  
+    if (!permission.granted) {
+      return (
+        <View style={styles.container}>
+          <Text style={{ textAlign: "center" }}>
+            We need your permission to use the camera
+          </Text>
+          <Button onPress={requestPermission} title="Grant permission" />
+        </View>
+      );
+    }
+  
+    const takePicture = async () => {
+      const photo = await ref.current?.takePictureAsync();
+      setUri(photo?.uri ?? null);
+    };
+  
+    const recordVideo = async () => {
+      if (recording) {
+        setRecording(false);
+        ref.current?.stopRecording();
+        return;
+      }
+      setRecording(true);
+      const video = await ref.current?.recordAsync();
+      console.log({ video });
+    };
+  
+    const toggleMode = () => {
+      setMode((prev) => (prev === "picture" ? "video" : "picture"));
+    };
+  
+    const toggleFacing = () => {
+      setFacing((prev) => (prev === "back" ? "front" : "back"));
+    };
+  
+    const renderPicture = () => {
+      return (
+        <View>
+          <Image
+            source={{ uri }}
+            contentFit="contain"
+            style={{ width: 300, aspectRatio: 1 }}
+          />
+          <Button onPress={() => setUri(null)} title="Take another picture" />
+        </View>
+      );
+    };
+  
+    const renderCamera = () => {
+      return (
+        <CameraView
+          style={styles.camera}
+          ref={ref}
+          mode={mode}
+          facing={facing}
+          mute={false}
+          responsiveOrientationWhenOrientationLocked
+        >
+          <View style={styles.shutterContainer}>
+            <Pressable onPress={toggleMode}>
+              {mode === "picture" ? (
+                <AntDesign name="picture" size={32} color="white" />
+              ) : (
+                <Feather name="video" size={32} color="white" />
+              )}
+            </Pressable>
+            <Pressable onPress={mode === "picture" ? takePicture : recordVideo}>
+              {({ pressed }) => (
+                <View
+                  style={[
+                    styles.shutterBtn,
+                    {
+                      opacity: pressed ? 0.5 : 1,
+                    },
+                  ]}
+                >
+                  <View
+                    style={[
+                      styles.shutterBtnInner,
+                      {
+                        backgroundColor: mode === "picture" ? "white" : "red",
+                      },
+                    ]}
+                  />
+                </View>
+              )}
+            </Pressable>
+            <Pressable onPress={toggleFacing}>
+              <FontAwesome6 name="rotate-left" size={32} color="white" />
+            </Pressable>
+          </View>
+        </CameraView>
+      );
+    };
+  
     return (
-      <View style={styles.container}>
-        <Text style={styles.message}>We need your permission to show the camera</Text>
-        <Button onPress={requestPermission} title="grant permission" />
+        <View style={styles.container}>
+        <Pressable style={styles.goBack} onPress={() => navigation.goBack()}>
+          <AntDesign name="arrowleft" size={32} color="white" />
+        </Pressable>
+        {uri ? renderPicture() : renderCamera()}
       </View>
     );
   }
-
-  function toggleCameraFacing() {
-    setFacing(current => (current === 'back' ? 'front' : 'back'));
-  }
-
-  return (
-    <View style={styles.container}>
-      <CameraView style={styles.camera} facing={facing}>
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
-            <Text style={styles.text}>Flip Camera</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress={() => navigation.goBack()}>
-            <Text style={styles.text}>Go Back</Text>
-          </TouchableOpacity>
-        </View>
-      </CameraView>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-  },
-  message: {
-    textAlign: 'center',
-    paddingBottom: 10,
-  },
-  camera: {
-    flex: 1,
-  },
-  buttonContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    backgroundColor: 'transparent',
-    margin: 64,
-  },
-  button: {
-    flex: 1,
-    alignSelf: 'flex-end',
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: 'white',
-  },
-});
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#fff",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    camera: {
+      flex: 1,
+      width: "100%",
+    },
+    shutterContainer: {
+      position: "absolute",
+      bottom: 44,
+      left: 0,
+      width: "100%",
+      alignItems: "center",
+      flexDirection: "row",
+      justifyContent: "space-between",
+      paddingHorizontal: 30,
+    },
+    shutterBtn: {
+      backgroundColor: "transparent",
+      borderWidth: 5,
+      borderColor: "white",
+      width: 85,
+      height: 85,
+      borderRadius: 45,
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    shutterBtnInner: {
+      width: 70,
+      height: 70,
+      borderRadius: 50,
+    },
+    goBack: {
+        position: 'absolute',
+        top: 40,
+        left: 20,
+        zIndex: 1,
+      },
+  });
